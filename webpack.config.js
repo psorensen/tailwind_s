160730efb24a3c3e3f7c25d3c46d49b65c89bd77
Peter Sorensen <peter@Peters-MacBook-Air.local>
2019-08-18 10:39:38 -0700

    formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: [
-	  './src/style.css',
-	  './src/index.js'
+	entry: [
+		'./src/style.css',
+		'./src/index.js'
 	],
-  mode: process.env.NODE_ENV,
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: { importLoaders: 1 } },
-            'postcss-loader',
-          ],
-        }),
-      },
-    ],
-  },
-  plugins: [
-    new ExtractTextPlugin('style.css', {
-      disable: process.env.NODE_ENV === 'development',
-    })
-  ],
+	mode: process.env.NODE_ENV,
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: [{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1
+						}
+					},
+					'postcss-loader',
+				],
+			}),
+		}, ],
+	},
+	plugins: [
+		new ExtractTextPlugin('style.css', {
+			disable: process.env.NODE_ENV === 'development',
+		})
+	],
 }
